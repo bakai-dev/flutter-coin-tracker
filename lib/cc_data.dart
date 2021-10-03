5441a4269744c4993942db8b4439976a387de7e4
bakaiDev <developer.bakai@gmail.com>
2021-10-04 03:41:41 +0600

    Get data form API

--- lib/cc_data.dart
+++ lib/cc_data.dart
@@ -2,7 +2,7 @@ class CCData {
   late String name;
   late String symbol;
   late int rank;
-  late double price;
+  late dynamic price;
 
   CCData({required this.name, required this.symbol, required this.rank, required this.price});
 }
--- lib/cc_list.dart
+++ lib/cc_list.dart
@@ -1,11 +1,20 @@
+import 'dart:convert';
+
 import 'package:c_tracker/cc_data.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class CCList extends StatelessWidget {
+class CCList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CCListState();
+  }
+}
 
+class CCListState extends State<CCList> {
   List<CCData> data = [
     CCData(name: 'Bit', symbol: 'BTC', rank: 1, price: 6000),
-    CCData(name: 'Et', symbol: 'ETC', rank: 2, price: 200.1)
+    CCData(name: 'Etsss', symbol: 'ETC', rank: 2, price: 200.1)
   ];
 
   @override
@@ -19,15 +28,47 @@ class CCList extends StatelessWidget {
           children: _buildList(),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () => _loadCC(),
+      ),
     );
   }
 
   List<Widget> _buildList() {
-    return data.map((CCData f) => ListTile(
-      subtitle: Text(f.symbol),
-      title: Text(f.name),
-      leading: CircleAvatar(child: Text(f.rank.toString())),
-      trailing: Text('\$${f.price.toString()}'),
-    )).toList();
+    return data
+        .map((CCData f) => ListTile(
+              subtitle: Text(f.symbol),
+              title: Text(f.name),
+              leading: CircleAvatar(child: Text(f.rank.toString())),
+              trailing: Text('\$${f.price.toString()}'),
+            ))
+        .toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCC();
+  }
+
+  _loadCC() async {
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd'));
+    if (response.statusCode == 200) {
+      var userData =
+          List<Map<String, dynamic>>.from(json.decode(response.body));
+
+      var ccDataList = <CCData>[];
+
+      userData.forEach((element) {
+        var record = CCData(name: element['name'], symbol: element['symbol'], rank: element['market_cap_rank'], price: element['current_price']);
+        ccDataList.add(record);
+      });
+
+      setState(() {
+        data = ccDataList;
+      });
+  }
   }
 }
